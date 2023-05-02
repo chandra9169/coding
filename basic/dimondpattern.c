9c03694c7dcf981201ff9f91cd7ad3b17c8318ad
chandra9169 <indraprataps001@gmail.com>
2023-05-02 23:24:13 +0530

    change in dimond pattern code

--- basic/dimondpattern.c
+++ basic/dimondpattern.c
@@ -11,12 +11,12 @@ void main()
     for ( i = 1; i < n; i++)
     {
  
-        for (j = 1; j < n-i; j++)
+        for (j = 0; j < n-i; j++)
             printf("  ");
   
    
         for (k = 0; k != 2*i-1; k++)
-            printf(" *");
+            printf("* ");
   
         printf("\n");
         
