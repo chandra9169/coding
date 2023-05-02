9350d0c2f23de5aa4502c0d39c373eccfe6919a7
chandra9169 <indraprataps001@gmail.com>
2023-05-02 23:34:45 +0530

    damaru pattern program

---
+++ basic/damarupattern.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+  
+
+void main()
+{
+    int i,j,k,n;
+    printf("enter the n:");
+    scanf("%d",&n);
+  
+
+    for ( i = n; i >= 1; i--)
+    {
+ 
+        for (j = 0; j < n-i; j++)
+            printf("  ");
+  
+   
+        for (k = 0; k != 2*i-1; k++)
+            printf("* ");
+  
+        printf("\n");
+        
+    }
+   for (i = 2; i <=n; ++i) {
+      for (j = 0; j < n - i; ++j)
+         printf("  ");
+      for (k = i; k <= 2 * i - 1; ++k)
+         printf("* ");
+      for (k = 0; k < i - 1; ++k)
+         printf("* ");
+      printf("\n");
+   }
+}
